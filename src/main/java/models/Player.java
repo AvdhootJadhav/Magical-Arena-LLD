246e27c744de5ae12bf96476d38203506cdb8caf
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-05-04 11:28:54 +0530

    created player model class

---
+++ src/main/java/models/Player.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(){}
+
+    public Player(int health, int strength, int attack){
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "health=" + health +
+                ", strength=" + strength +
+                ", attack=" + attack +
+                '}';
+    }
+}
